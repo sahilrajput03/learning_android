92899b5d10be3c6bcd503e2c9a39a5372b2bd85a
Sahil Rajput <sahilrajput03@gmail.com>
2022-01-03 00:28:12 +0530

    Add notification example bare.

--- hello_world_sahil/README.md
+++ hello_world_sahil/README.md
@@ -1,4 +1,15 @@
-Initial code of MainActivity.java
+- Folder structure for image sizes:
+
+```
+android/app/src/main/res/drawable-mdpi/ (24x24)
+android/app/src/main/res/drawable-hdpi/ (36x36)
+android/app/src/main/res/drawable-xhdpi/ (48x48)
+android/app/src/main/res/drawable-xxhdpi/ (72x72)
+android/app/src/main/res/drawable-xxxhdpi/ (96x96)
+android/app/src/main/res/drawable-xxxhdpi/ (256x256) (Large Icon)
+```
+
+- Initial code of MainActivity.java
 
 ```java
 package com.helloworld;
--- hello_world_sahil/app/src/main/java/com/helloworld/MainActivity.java
+++ hello_world_sahil/app/src/main/java/com/helloworld/MainActivity.java
@@ -6,7 +6,14 @@ import android.widget.Toast;
 
 // NotificationCompat, src: https://stackoverflow.com/a/59507099/10012446
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+import android.app.PendingIntent; // src: https://stackoverflow.com/a/27396662/10012446
+import android.content.Intent; // src: https://stackoverflow.com/a/26246956/10012446
+import android.app.NotificationChannel; // src: https://stackoverflow.com/q/46155562/10012446
+import android.app.NotificationManager;
+import android.app.Notification;
+import android.content.Context;
+import android.os.Build;
+
 
 public class MainActivity extends Activity {
   @Override
@@ -17,16 +24,44 @@ public class MainActivity extends Activity {
     // Sending a toast message
     Toast.makeText(getApplicationContext(), "Toast: Activity Code just executed.", Toast.LENGTH_LONG).show(); // Toast.LENGTH_SHORT and Toast.LENGTH_LONG,  // We can't change duration at all, source: https://stackoverflow.com/a/2221285/10012446
 
-    // Sending a statusbar notification: docs:https://developer.android.com/training/notify-user/build-notification#add_the_support_library
-    NotificationCompat.Builder b = new NotificationCompat.Builder(this, "my_channel_id1") // docs: https://developer.android.com/reference/androidx/core/app/NotificationCompat.Builder
-        .setSmallIcon(R.drawable.ic_launcher_background) // this is any desirable icon.
-        .setContentTitle("textTitle")
-        .setContentText("textContent")
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-    NotificationManagerCompat nm = NotificationManagerCompat.from(this);
+    // Sending a statusbar notification: docs:https://developer.android.com/training/notify-user/build-notification#add_the_support_library.
+    // src: https://stackoverflow.com/a/16448278/10012446
+    NotificationManager mNotificationManager;
+
+    NotificationCompat.Builder b = new NotificationCompat.Builder(getApplicationContext(), "notify_001");
+    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+    PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0, i, 0);
+
+    NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+    bigText.bigText("My long notification text here goes here..");
+    bigText.setBigContentTitle("Today's Bible Verse");
+    bigText.setSummaryText("Text in detail");
+
+    b.setContentIntent(pi);
+    b.setSmallIcon(R.mipmap.ic_launcher_round);
+    b.setContentTitle("Your Title");
+    b.setContentText("Your text");
+    b.setPriority(Notification.PRIORITY_MAX);
+    b.setStyle(bigText);
+
+    mNotificationManager =
+        (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+    // === Removed some obsoletes
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+    {
+        String channelId = "Your_channel_id";
+        NotificationChannel channel = new NotificationChannel(
+                                            channelId,
+                                            "Channel human readable title",
+                                            NotificationManager.IMPORTANCE_HIGH);
+      mNotificationManager.createNotificationChannel(channel);
+      b.setChannelId(channelId);
+    }
+
+    mNotificationManager.notify(0, b.build());
     
-    int notificationId = 101; // notificationId is a unique int for each notification that you must define
-    nm.notify(notificationId, b.build());
+    
 
     // URL url = new URL("https://i.picsum.photos/id/202/536/354.jpg?hmac=yO3GuY1TtL9JGeHctCd_n_LRx7XrmqDHOMESFbbN5lM");
     // Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
